9915a178293649fffce0fa0b2e6076090b0790dc
Alejandro Valera <ale24vv@gmail.com>
2025-06-28 02:30:54 -0400

    fix backgroud stars

--- src/components/sections/Background.tsx
+++ src/components/sections/Background.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import { CSSProperties } from 'react'
 import { useStars, Star } from '@/utils/stars'
--- src/utils/stars.ts
+++ src/utils/stars.ts
@@ -11,26 +11,26 @@ export const useStars = (starCount: number = 15, heightRatio: number = 0.6) => {
 
   const createStars = () => {
     const width = typeof window !== 'undefined' ? window.innerWidth : 1920;
-    const height = typeof window !== 'undefined' ? window.innerHeight * heightRatio : 1080 * heightRatio;
-    const newStars: Star[] = [];
+    const height = typeof window !== 'undefined' ? window.innerHeight * heightRatio : 1080 * heightRatio
+    const newStars: Star[] = []
 
     for (let i = 0; i < starCount; i++) {
       newStars.push({
         id: i,
         x: Math.floor(Math.random() * width),
         y: Math.floor(Math.random() * height),
-      });
+      })
     }
 
-    setStars(newStars);
-  };
+    setStars(newStars)
+  }
 
   useEffect(() => {
-    createStars();
-    const updateStars = () => createStars();
-    window.addEventListener('resize', updateStars);
-    return () => window.removeEventListener('resize', updateStars);
-  }, []);
+    createStars()
+    const updateStars = () => createStars()
+    window.addEventListener('resize', updateStars)
+    return () => window.removeEventListener('resize', updateStars)
+  }, [])
 
-  return stars;
-};
+  return stars
+}
