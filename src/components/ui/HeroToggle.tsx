38552c9b736e291ac4e6f878b46293c573e96ea3
Alejandro Valera <ale24vv@gmail.com>
2025-07-06 01:07:57 -0400

    herotoogle fix

--- src/components/ui/HeroToggle.tsx
+++ src/components/ui/HeroToggle.tsx
@@ -6,7 +6,6 @@ export default function HeroToggle({ isOpen }: {isOpen : boolean}) {
     window.scrollTo({ top: 600, behavior: 'smooth' })
   }
 
-
   return (
     <>
       <div className="fixed z-60 top-10 left-10">
@@ -16,7 +15,7 @@ export default function HeroToggle({ isOpen }: {isOpen : boolean}) {
           aria-label="Toggle menu"
         >
           <span className={`inline-block transition-opacity duration-300 ${isOpen ? 'opacity-0' : 'opacity-100'}`}>
-            <h3 className="title text-3xl tracking-[-0.075em] text-indigo-200">AV</h3>
+            <h3 className="title text-2xl md:text-3xl tracking-[-0.075em] text-indigo-200">AV</h3>
           </span>
         </button>
       </div>
