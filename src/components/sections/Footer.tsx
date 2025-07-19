1b1c402fb1398ac56b2eb526513439a86b0bdca3
Alejandro Valera <ale24vv@gmail.com>
2025-07-19 17:56:43 -0400

    fix cv pdf

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -20,7 +20,9 @@ export default function Footer() {
 
         <div className="flex flex-col md:flex-row bg-gray-950/60 border rounded-lg border-purple-900 shadow-purple-900/80 p-6 gap-4 items-center">
           <a
-            href="https://drive.google.com/drive/folders/1xe8sUbS3ZVCWrYPO9ETUzqaDT9aJTtHi?usp=sharing"
+            href="/cv.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
             className="text-indigo-400 hover:text-indigo-200 text-sm md:text-base transition-colors duration-300 flex items-center gap-2"
           >
             <ArrowDownCircleIcon className="h-7 w-7 text-gray-200" />
